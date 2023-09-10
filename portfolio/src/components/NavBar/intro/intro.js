530b2b7a7685f9fb9eea7540dd6c713f3c6a1baf
[Vidit Pandey] <viditpandey06@gmail.com>
2023-09-10 11:45:04 +0530

    Updated Intro section

--- portfolio/src/components/NavBar/intro/intro.js
+++ portfolio/src/components/NavBar/intro/intro.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import "./intro.css";
 import bg from "../../../assests/image.png";
+import btnImg from "../../../assests/hireme.png";
+import { Link } from "react-scroll";
 const Intro = () => {
   return (
    <section id="intro">
     <div className="introContent">
        <span className="hello">Hello,</span>
-       <br />
-       <span className="introText"></span>
+       <span className="introText">I'm <span className="introName">Vidit</span><br />Software Developer </span>
+        <p className="introPara">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, totam. <br /> Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, sint.</p>
+        <Link><button className="btn"><img src={btnImg} alt="Hire"className='btn.Img' /> Hire Me</button></Link>
     </div>
     <img src={bg} alt="Photo" className="bg" />
    </section>
